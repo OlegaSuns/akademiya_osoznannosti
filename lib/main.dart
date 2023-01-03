e3340d752d96031797687446bad2024a7ac7f787
OlegaSuns <oleg18@mail.ru>
2023-01-03 21:46:23 +0700

    01_структура

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:akademiya_osoznannosti/pages/01_home.dart';
+import 'package:akademiya_osoznannosti/pages/02_info_about_rector.dart';
+import 'package:akademiya_osoznannosti/pages/03_institutes.dart';
+import 'package:akademiya_osoznannosti/pages/04_info_about_akademiya.dart';
+import 'package:akademiya_osoznannosti/pages/05_courses.dart';
+import 'package:akademiya_osoznannosti/pages/06_sign_up_cours.dart';
+
+
 
 void main() => runApp(MaterialApp(
   theme: ThemeData(
     primaryColor: Colors.deepOrangeAccent,
 
   ),
-  home: const Text('Hello'),
+  home: Courses(),
 ));
 
 
